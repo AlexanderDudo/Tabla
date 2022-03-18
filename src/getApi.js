7b5052061bc5b349bcf7aded5aa1ad4ef4f3825b
AlexanderDudo <2019030289@upsin.edu.mx>
2022-03-17 21:57:38 -0700

    primer commit

---
+++ src/getApi.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react"
+
+const AsyncAwait = () => {
+  const [producto, setProducto] = useState([])
+
+  const fetchData = async () => {
+    const response = await fetch("http://scratchya.com.ar/react/datos.php")
+    const data = await response.json()
+    setProducto(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  return (
+    <div>
+      <table class='tg'>
+          <thead>
+              <tr>
+              <td>Código</td>
+              <td>Descripción</td>
+              <td>Precio</td>
+              </tr>
+        </thead>
+        <tbody>
+          {producto.map(producto => (
+            <tr>
+              <td>{producto.codigo}</td>
+              <td>{producto.descripcion}</td>
+              <td>{producto.precio}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default AsyncAwait;
